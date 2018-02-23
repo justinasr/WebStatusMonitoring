e44d41ec25863a8faeb04de33749af28e3bf7bed
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2018-02-23 18:53:06 +0100

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -s 'http://instance4:5000/update_status'
+kinit jrumsevi@CERN.CH -f -k -t keytab.keytab
+
+rm /home/WebStatusMonitoring/cookies/prod-cookie
+rm /home/WebStatusMonitoring/cookies/dev-cookie
+
+cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o /home/WebStatusMonitoring/cookies/prod-cookie --krb
+cern-get-sso-cookie -u https://cms-pdmv-dev.cern.ch/mcm/ -o /home/WebStatusMonitoring/cookies/dev-cookie --krb
+
+curl -s 'http://localhost:5000/update_status'
