b61e87f17ca3b2880098de47cf85e2540347aafe
Justinas Rumševičius <justinas.rumsevicius@cern.ch>
2018-05-24 15:39:51 +0200

    Update debug-mode flag

--- status_checker/main.py
+++ status_checker/main.py
@@ -41,7 +41,7 @@ def index_simple(name=None):
                            targets=targets,
                            all_logs=all_logs,
                            version=version,
-                           debug=bool(config.get('debug-mode', False)))
+                           debug=bool(int(config.get('debug-mode', 0))))
 
 
 @app.route('/python_version')
@@ -74,7 +74,7 @@ def run_flask():
     logger = logging.getLogger('logger')
     logger.info('Starting app...')
     config = read_config()
-    debug_mode = bool(config.get('debug-mode', False))
+    debug_mode = bool(int(config.get('debug-mode', 0)))
     port = int(config.get('port', 80))
     app.run(host='0.0.0.0',
             port=port,
